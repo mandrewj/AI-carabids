9e9a9a3a37ac2faa94d42e71f60f528d1384bd53
Andrew Johnston <ajohnston@asu.edu>
2023-08-22 15:29:02 -0700

    Update 5sp.php

--- 5sp.php
+++ 5sp.php
@@ -1,30 +1,26 @@
 <?php
-// Check if the form has been submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if the "url" POST variable is set
-    if (isset($_GET["url"])) {
-        // Get the submitted URL
-        $submittedUrl = $_POST["url"];
-        echo $submittedUrl;
-        // Include the class file
-        require_once "classes/URLProcessor.php";
+$url = array_key_exists('url',$_REQUEST)?$_REQUEST['url']:0;
+// Check if the "url" REQUEST variable is set
+if ($url != 0) {
+	// Get the submitted URL
+	$submittedUrl = $_REQUEST["url"];
+	echo $submittedUrl;
+	// Include the class file
+	require_once "classes/URLProcessor.php";
 
-        // Create an instance of the URLProcessor class
-        $urlProcessor = new URLProcessor();
+	// Create an instance of the URLProcessor class
+	$urlProcessor = new URLProcessor();
 
-        // Process the submitted URL
-        $detoutput = $urlProcessor->processURL($submittedUrl);
+	// Process the submitted URL
+	$detoutput = $urlProcessor->processURL($submittedUrl);
 
-        // Output the processed URL
-		header('Content-type: application/json');
-		echo json_encode( $data );
-        }
-    else{
-    	echo 'No URL submitted.';
-    	echo 'Usage: 5sp.php?url=path/to/image.jpg';
-    	}
-    }
+	// Output the processed URL
+	header('Content-type: application/json');
+	echo json_encode( $data );
+	}
 else{
-	echo 'no get statements given';
-	echo 'Usage: 5sp.php?url=path/to/image.jpg';}
+	echo 'No URL submitted.';
+	echo 'Usage: 5sp.php?url=path/to/image.jpg';
+	}
+
 ?>
