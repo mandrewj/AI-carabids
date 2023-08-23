a4f9d491078842287b4199e93d561b8480cc1836
Andrew Johnston <ajohnston@asu.edu>
2023-08-22 17:21:36 -0700

    Update URLProcessor.php

--- classes/URLProcessor.php
+++ classes/URLProcessor.php
@@ -6,11 +6,11 @@ class URLProcessor {
         $sanitizedUrl = filter_var($url, FILTER_SANITIZE_URL);
 
         // Execute the Python script and get the output
-        $pythonScript = 'parse.py';
-        $command = "python3 $pythonScript " . escapeshellcmd($sanitizedUrl) . " 2>$1";
-        print $command ;
+        $pythonScript = '/var/www/html/AI-carabids/parse.py';
+        $command = "/usr/bin/python3 $pythonScript " . escapeshellcmd($sanitizedUrl) . " 2>$1";
+        print $command;
         $output = shell_exec($command);
-                print "output ran";
+        print "output ran";
 
         // Return the output
         //header('Content-Type: application/json');
