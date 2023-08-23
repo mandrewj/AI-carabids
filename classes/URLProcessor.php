d4978faf9e690de83d6adf2f2394fb54ebf713d4
Andrew Johnston <ajohnston@asu.edu>
2023-08-23 12:16:01 -0700

    Update URLProcessor.php

--- classes/URLProcessor.php
+++ classes/URLProcessor.php
@@ -1,12 +1,12 @@
 <?php
 
 class URLProcessor {
-    public function processURL($url, $pythonScript = NULL) {
+    public function processURL($url, $pythonScript = 0) {
         // Sanitize the URL (you may want to perform more validation)
         $sanitizedUrl = filter_var($url, FILTER_SANITIZE_URL);
 
         // Execute the Python script and get the output
-        if ($pythonScript = NULL){
+        if ($pythonScript = 0){
         	$pythonScript = '/var/www/html/AI-carabids/5sp_det.py';
         }
         $command = "/usr/bin/python3 $pythonScript " . escapeshellcmd($sanitizedUrl) . " 2>&1";
