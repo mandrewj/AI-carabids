2972eccd797f70f5f6724e6c4fb15931fdebf7c7
Andrew Johnston <ajohnston@asu.edu>
2023-08-23 09:07:08 -0700

    add log functionality

--- parse.py
+++ parse.py
@@ -21,7 +21,7 @@ def parse_url(url):
     import cv2, json
     import numpy as np
     from tensorflow.keras.models import load_model
-    from datetime import date
+    from datetime
     #update this path to the desired model
     model = load_model(os.path.join(path,'models','five_species.tf'))
 	#re-read image in, resize it per model expectations
@@ -32,19 +32,30 @@ def parse_url(url):
     y_classes = y_prob.argmax(axis=-1)
     #define class names - could make this programmatic in the future
     classes=('PASCAL','OMUDEJ','PASELO','PTELAM','SCASUB')
+    #write output to log file
+    log_file = os.path.join(path,'logs','5sp.log')
     #Create output and print it
     #print(f'Predicted taxon is: {classes[y_classes[0]]}')
     #create dictionary of dwc terms
     dwc_det = {
             "scientificName" : classes[y_prob.argmax()],
             "identifiedBy" : "AI model for carabids by M.A. Johnston",
-            "dateIdentified" : date.today().strftime("%Y-%m-%d"),
+            "dateIdentified" : datetime.date.today().strftime("%Y-%m-%d"),
             "identificationReferences" : "ID used the five_species carabid model",
             "identificationRemarks" : f'Model probability is {y_prob[0][y_prob.argmax()]}'
         }
+    #write JSON file to return
     json_file=os.path.join(path,'data.json')
     with open(json_file, 'w') as f:
     	json.dump(dwc_det, f, indent=4)
+    #write output to log file
+    log_file = os.path.join(path,'logs','5sp.log')
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    x = json.dumps(dwc_det)
+    log_entry = f"[{timestamp}] {x}\n"
+    with open(log_file, "a") as lf:
+    	lf.write(log_entry)
+    #return filename to finish script
     return(json_file)
     
 
